ce6746034fef864c894e444cdc311a5908bedecd
Maddie Adair <98988491+maddieadair@users.noreply.github.com>
2024-04-19 17:10:12 -0500

    add low stock trigger alert

--- src/pages/shopManager/ShopManagerHome.js
+++ src/pages/shopManager/ShopManagerHome.js
@@ -7,6 +7,7 @@ import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
 import { MdOutlineDelete } from "react-icons/md";
 import { MdOutlineCheckBox } from "react-icons/md";
 import { IoClose } from "react-icons/io5";
+import { HiBellAlert } from "react-icons/hi2";
 
 export default function ShopManagerHome() {
   const [revenue, setRevenue] = useState([]);
@@ -15,6 +16,7 @@ export default function ShopManagerHome() {
   const [lowStock, setLowStock] = useState([]);
   const [bestsellers, setBestsellers] = useState([]);
   const [worstsellers, setWorstsellers] = useState([]);
+  const [openView, setOpenView] = useState(false);
 
   useEffect(() => {
     fetchRevenue();
@@ -81,6 +83,9 @@ export default function ShopManagerHome() {
       })
       .then((data) => {
         setLowStock(data);
+        if (data.length > 0) {
+          setOpenView(true);
+        }
       });
   };
 
@@ -112,106 +117,147 @@ export default function ShopManagerHome() {
         <div className="flex flex-col gap-y-8 w-full h-full pb-14">
           <ShopManagerBar title="Dashboard" />
 
-        {revenue !== null && soldOut.length > 0 && lowStock.length > 0 && bestsellers.length > 0 && worstsellers.length > 0 ? 
-          <div className="flex flex-row px-14 gap-x-6 text-md">
-            <div className="flex flex-col gap-y-6 w-1/2">
-              {revenue !== null ? (
-                <div className="border rounded-md p-6 flex flex-col gap-y-4 h-fit">
-                  <h1 className="font-bold text-xl">Total Shop Revenue</h1>
-                  <h4 className="text-xl">${revenue}</h4>
-                </div>
-              ) : null}
-
-              {soldOut.length > 0 ? (
-                <div className="border rounded-md p-6 flex flex-col gap-y-6 h-fit">
-                  <p className="font-bold text-xl">Sold Out Items</p>
-                  <div className="flex flex-row justify-between">
-                    <p className="font-bold underline">Item</p>
+          {revenue !== null &&
+          soldOut.length > 0 &&
+          lowStock.length > 0 &&
+          bestsellers.length > 0 &&
+          worstsellers.length > 0 ? (
+            <div className="flex flex-row px-14 gap-x-6 text-md">
+              <div className="flex flex-col gap-y-6 w-1/2">
+                {revenue !== null ? (
+                  <div className="border rounded-md p-6 flex flex-col gap-y-4 h-fit">
+                    <h1 className="font-bold text-xl">Total Shop Revenue</h1>
+                    <h4 className="text-xl">${revenue}</h4>
                   </div>
+                ) : null}
 
-                  <div className="flex flex-col gap-y-2">
-                    {soldOut.map((item, id) => (
-                      <div
-                        key={item.gift_index}
-                        className="flex flex-row justify-between"
-                      >
-                        <p className="">{item.gift_name}</p>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              ) : null}
+                {soldOut.length > 0 ? (
+                  <div className="border rounded-md p-6 flex flex-col gap-y-6 h-fit">
+                    <p className="font-bold text-xl">Sold Out Items</p>
+                    <div className="flex flex-row justify-between">
+                      <p className="font-bold underline">Item</p>
+                    </div>
 
-              {lowStock.length > 0 ? (
-                <div className="border rounded-md p-6 flex flex-col gap-y-6 h-fit">
-                  <p className="font-bold text-xl">Items Low on Stock</p>
-                  <div className="flex flex-row justify-between">
-                    <p className="font-bold underline">Item</p>
-                    <p className="font-bold underline"># of stock left</p>
+                    <div className="flex flex-col gap-y-2">
+                      {soldOut.map((item, id) => (
+                        <div
+                          key={item.gift_index}
+                          className="flex flex-row justify-between"
+                        >
+                          <p className="">{item.gift_name}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
+                ) : null}
 
-                  <div className="flex flex-col gap-y-2">
-                    {lowStock.map((item, id) => (
-                      <div
-                        key={item.gift_index}
-                        className="flex flex-row justify-between"
-                      >
-                        <p className="">{item.gift_name}</p>
-                        <p className="">{item.gift_currStock}</p>
-                      </div>
-                    ))}
+                {lowStock.length > 0 ? (
+                  <div className="border rounded-md p-6 flex flex-col gap-y-6 h-fit">
+                    <p className="font-bold text-xl">Items Low on Stock</p>
+                    <div className="flex flex-row justify-between">
+                      <p className="font-bold underline">Item</p>
+                      <p className="font-bold underline"># of stock left</p>
+                    </div>
+
+                    <div className="flex flex-col gap-y-2">
+                      {lowStock.map((item, id) => (
+                        <div
+                          key={item.gift_index}
+                          className="flex flex-row justify-between"
+                        >
+                          <p className="">{item.gift_name}</p>
+                          <p className="">{item.gift_currStock}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                </div>
-              ) : null}
+                ) : null}
+              </div>
+
+              <div className="flex flex-col gap-y-6 w-1/2">
+                {bestsellers.length > 0 ? (
+                  <div className="border rounded-md p-6 flex flex-col gap-y-6 h-fit">
+                    <p className="font-bold text-xl">Bestsellers</p>
+                    <div className="flex flex-row justify-between">
+                      <p className="font-bold underline">Item</p>
+                      <p className="font-bold underline"># sold</p>
+                    </div>
+
+                    <div className="flex flex-col gap-y-2">
+                      {bestsellers.map((item, id) => (
+                        <div
+                          key={item.gift_index}
+                          className="flex flex-row justify-between"
+                        >
+                          <p className="">{item.gift_name}</p>
+                          <p className="">{item.gift_numSold}</p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ) : null}
+
+                {worstsellers.length > 0 ? (
+                  <div className="border rounded-md p-6 flex flex-col gap-y-6 h-fit">
+                    <p className="font-bold text-xl">Worstsellers</p>
+                    <div className="flex flex-row justify-between">
+                      <p className="font-bold underline">Item</p>
+                      <p className="font-bold underline"># sold</p>
+                    </div>
+
+                    <div className="flex flex-col gap-y-2">
+                      {worstsellers.map((item, id) => (
+                        <div
+                          key={item.gift_index}
+                          className="flex flex-row justify-between"
+                        >
+                          <p className="">{item.gift_name}</p>
+                          <p className="">{item.gift_numSold}</p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ) : null}
+              </div>
             </div>
+          ) : null}
 
-            <div className="flex flex-col gap-y-6 w-1/2">
-              {bestsellers.length > 0 ? (
-                <div className="border rounded-md p-6 flex flex-col gap-y-6 h-fit">
-                  <p className="font-bold text-xl">Bestsellers</p>
-                  <div className="flex flex-row justify-between">
-                    <p className="font-bold underline">Item</p>
-                    <p className="font-bold underline"># sold</p>
-                  </div>
-
-                  <div className="flex flex-col gap-y-2">
-                    {bestsellers.map((item, id) => (
-                      <div
-                        key={item.gift_index}
-                        className="flex flex-row justify-between"
-                      >
-                        <p className="">{item.gift_name}</p>
-                        <p className="">{item.gift_numSold}</p>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              ) : null}
-
-              {worstsellers.length > 0 ? (
-                <div className="border rounded-md p-6 flex flex-col gap-y-6 h-fit">
-                  <p className="font-bold text-xl">Worstsellers</p>
-                  <div className="flex flex-row justify-between">
-                    <p className="font-bold underline">Item</p>
-                    <p className="font-bold underline"># sold</p>
-                  </div>
-
-                  <div className="flex flex-col gap-y-2">
-                    {worstsellers.map((item, id) => (
-                      <div
-                        key={item.gift_index}
-                        className="flex flex-row justify-between"
-                      >
-                        <p className="">{item.gift_name}</p>
-                        <p className="">{item.gift_numSold}</p>
-                      </div>
-                    ))}
+          {lowStock.length > 0 && openView ? (
+            <div className="bg-black fixed h-screen w-screen z-30 top-0 left-0 bg-opacity-45 justify-center items-center flex overflow-hidden">
+              <div className="bg-white rounded-3xl h-fit w-1/2 shadow-md flex flex-col">
+                <div className="flex flex-col">
+                  <IoClose
+                    className="self-end size-8 m-6 mb-2 hover:cursor-pointer"
+                    onClick={() => {
+                      setOpenView(false);
+                    }}
+                  />
+                  <div className="flex flex-col px-6 pb-6 gap-y-6 divide-y-2">
+                    <div className="flex flex-col gap-y-4 text-center items-center justify-center">
+                        <HiBellAlert className="text-cinnabar" size={30}/>
+                        <h3 className="text-cinnabar text-xl font-bold">Urgent</h3>
+                        <p className="text-cinnabar text-lg">These items are currently low in stock</p>
+                    </div>
+                    <div className="flex flex-col gap-y-2 pt-6">
+                    <div className="flex flex-row justify-between">
+                      <p className="font-bold underline">Item</p>
+                      <p className="font-bold underline"># of stock left</p>
+                    </div>
+                      {lowStock.map((item, id) => (
+                        <div
+                          key={item.gift_index}
+                          className="flex flex-row justify-between"
+                        >
+                          <p className="">{item.gift_name} (#{item.gift_index})</p>
+                          <p className="">{item.gift_currStock}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
-              ) : null}
+              </div>
             </div>
-          </div>
-          : null }
+          ) : null}
         </div>
       </div>
     </div>
